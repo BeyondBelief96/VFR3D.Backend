c178eab84a9c77fd325c930a083fc540578f32f9
bberisford <brandonberisford@gmail.com>
2025-02-10 12:06:43 -0600

    changing airport job cron job service to run on correct interval

--- VFR3D.Infrastructure/Jobs/AirportsJob.cs
+++ VFR3D.Infrastructure/Jobs/AirportsJob.cs
@@ -14,7 +14,7 @@ namespace VFR3D.Infrastructure.Jobs
         public AirportsJob(
             IServiceProvider serviceProvider,
             ILogger<AirportsJob> logger)
-            : base("*/2 * * * *", TimeZoneInfo.Utc, logger)
+            : base("0 0 * * *", TimeZoneInfo.Utc, logger)
         {
             _serviceProvider = serviceProvider;
         }
