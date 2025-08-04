dabab0100d97c541b9ae37b8e714fdd5926dcecf
Brandon Berisford <brandonberisford@gmail.com>
2025-08-04 10:21:16 -0700

    Add weather-related properties to Metar class
    Updated the `Metar` class in `Metar.cs` to include new properties for temperature, dewpoint, wind speed, wind gusts, altimeter settings, quality control flags, flight category, maximum and minimum temperatures, expected precipitation over various time frames, snow depth, and vertical visibility. Each property includes a summary comment for clarity.

--- VFR3D.Domain/Entities/Metar.cs
+++ VFR3D.Domain/Entities/Metar.cs
@@ -41,9 +41,15 @@ public class Metar
     [Column("longitude")]
     public float? Longitude { get; set; }
 
+    /// <summary>
+    /// Temperature in degrees Celsius ex: 14.4
+    /// </summary>
     [Column("temp_c")]
     public float? TempC { get; set; }
 
+    /// <summary>
+    /// Dewpoint temperature in degrees Celsius ex: 2.8
+    /// </summary>
     [Column("dewpoint_c")]
     public float? DewpointC { get; set; }
 
@@ -53,9 +59,15 @@ public class Metar
     [Column("wind_dir_degrees")]
     public string? WindDirDegrees { get; set; }
 
+    /// <summary>
+    /// Wind speed in knots.
+    /// </summary>
     [Column("wind_speed_kt")]
     public int? WindSpeedKt { get; set; }
 
+    /// <summary>
+    /// Wind gust speed in knots, if available.
+    /// </summary>
     [Column("wind_gust_kt")]
     public int? WindGustKt { get; set; }
 
@@ -65,12 +77,21 @@ public class Metar
     [Column("visibility_statute_mi")]
     public string? VisibilityStatuteMi { get; set; }
 
+    /// <summary>
+    /// Altimeter setting in inches of mercury ex: 29.92
+    /// </summary>
     [Column("altim_in_hg")]
     public float? AltimInHg { get; set; }
 
+    /// <summary>
+    /// Altimer setting in millibars ex: 1013.25
+    /// </summary>
     [Column("sea_level_pressure_mb")]
     public float? SeaLevelPressureMb { get; set; }
 
+    /// <summary>
+    /// Quality control flags associated with the METAR data.
+    /// </summary>
     [Column("quality_control_flags", TypeName = "jsonb")]
     public MetarQualityControlFlags? QualityControlFlags { get; set; }
 
@@ -83,39 +104,71 @@ public class Metar
     [Column("sky_condition", TypeName = "jsonb")]
     public List<MetarSkyCondition>? SkyCondition { get; set; }
 
+    /// <summary>
+    /// Flight category, if available. ex: VFR, MVFR, IFR, LIFR
+    /// </summary>
     [Column("flight_category")]
     public string? FlightCategory { get; set; }
 
     [Column("three_hr_pressure_tendency_mb")]
     public float? ThreeHrPressureTendencyMb { get; set; }
 
+    /// <summary>
+    /// Maximum temperature in degrees Celsius, if available. ex: 15.0
+    /// </summary>
     [Column("maxT_c")]
     public float? MaxTC { get; set; }
 
+    /// <summary>
+    /// Minimum temperature in degrees Celsius, if available. ex: 3.0
+    /// </summary>
     [Column("minT_c")]
     public float? MinTC { get; set; }
 
+    /// <summary>
+    /// Maximum temperature in degrees Celsius predicted over the next 24 hours, if available. ex: 15.0
+    /// </summary>
     [Column("maxT24hr_c")]
     public float? MaxT24hrC { get; set; }
 
+    /// <summary>
+    /// Minimum temperature in degrees Celsius predicted over the next 24 hours, if available. ex: 3.0
     [Column("minT24hr_c")]
     public float? MinT24hrC { get; set; }
 
+    /// <summary>
+    /// Expected precipitation in inches over the last hour, if available. ex: 0.0
+    /// </summary>
     [Column("precip_in")]
     public float? PrecipIn { get; set; }
 
+    /// <summary>
+    /// Expected precipitation in inches over the next 3 hour, if available. ex: 0.0
+    /// </summary>
     [Column("pcp3hr_in")]
     public float? Pcp3hrIn { get; set; }
 
+    /// <summary>
+    /// Expected precipitation in inches over the next 6 hours, if available. ex: 0.0
+    /// </summary>
     [Column("pcp6hr_in")]
     public float? Pcp6hrIn { get; set; }
 
+    /// <summary>
+    /// Expected precipitation in inches over the next 24 hours, if available. ex: 0.0
+    /// </summary>
     [Column("pcp24hr_in")]
     public float? Pcp24hrIn { get; set; }
 
+    /// <summary>
+    /// Snow depth in inches, if available. ex: 0.0
+    /// </summary>
     [Column("snow_in")]
     public float? SnowIn { get; set; }
 
+    /// <summary>
+    /// Vertical visibility in feet, if available. ex: 1000
+    /// </summary>
     [Column("vert_vis_ft")]
     public int? VertVisFt { get; set; }
 
